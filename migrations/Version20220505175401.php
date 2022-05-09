f6eb84947e4220115fe6356f727467b70d16f431
paduCostiy <padu.costi@yahoo.com>
2022-05-09 10:04:48 +0300

    update

---
+++ migrations/Version20220505175401.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220505175401 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pastebin DROP name, DROP text, DROP city, CHANGE email email VARCHAR(255) DEFAULT NULL, CHANGE password password VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pastebin ADD name VARCHAR(55) NOT NULL, ADD text VARCHAR(250) NOT NULL, ADD city VARCHAR(250) NOT NULL, CHANGE email email VARCHAR(250) NOT NULL, CHANGE password password VARCHAR(55) NOT NULL');
+    }
+}
