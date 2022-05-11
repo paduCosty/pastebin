d1f8059712b63626f6a65c6bc84582a7c6153ba1
paduCostiy <padu.costi@yahoo.com>
2022-05-11 16:12:57 +0300

    navbar

---
+++ pastebin/src/Controller/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Home extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('/main/header.html.twig');
+    }
+}
